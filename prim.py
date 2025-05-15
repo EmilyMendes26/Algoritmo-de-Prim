a356e7087febebd88c6a2afb77a4254c9de590f2
EmilyM <emilymendes26@hotmail.com>
2025-05-14 23:21:41 -0300

    codigo grafo funcional, falta concertar peso do no visitado

--- prim.py
+++ prim.py
@@ -2,23 +2,52 @@ import heapq
 from heapq import heappop, heappush
 
 grafo = {
-    'A': [('D',0),('B',1)],
-    'B': [('D',1),('C',2)],
-    'C': [('F',3)],
-    'D': [('E',4), ('B',5)],
-    'E': [('C',6)],
-    'F': [('C',0),('E',5)]
+    'A': [('B', 1), ('D', 3)],
+    'B': [('A', 1), ('C', 2), ('D', 4)],
+    'C': [('B', 2), ('D', 5), ('E', 1)],
+    'D': [('A', 3), ('B', 4), ('C', 5), ('E', 2)],
+    'E': [('C', 1), ('D', 2)]
 }
 
-folhas = []
-mst = []
-visitados = []
+def prim(grafo,pai):
+    folhas = []
+    visitados = []
+    mst = []
+    custo = 0
 
-for pai in grafo:
-    for destino, peso in grafo[pai]:
-        folha = (peso, destino, pai)
-        folhas.append(folha)
+    visitados.append(pai)
+    for pai in grafo:
+        for no, peso in grafo[pai]:
+            aresta = (peso, pai, no)
+            heappush(folhas,aresta)
 
-for folha in folhas:
-    print(folha)
+    while folhas:
+        peso, pai, no = heappop(folhas)
+
+        if no not in visitados:
+            visitados.append(no)
+            mst.append((peso,pai,no))
+            custo += peso
+
+            for prox_no, heuristica in grafo[no]:
+                if prox_no not in visitados:
+                    nova_folha = (heuristica, no, prox_no)
+                    heappush(folhas,nova_folha)
+    return mst, custo, visitados
+
+
+mst,custo,visitados = prim(grafo,'D')
+
+print("Arvore geradora minima:")
+print(mst)
+
+for aresta in mst:
+    peso, pai, no = aresta
+    print(f"No visitado: {pai} | Proximo no: {no} | Peso: {peso}")
+
+print (f"Custo total: {custo}")
+print (visitados)
+
+
+
 
